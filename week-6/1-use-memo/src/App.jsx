3bfcbad9fca329e6a33682403a4aabefe0a86f5d
sam-the-dev <thedev.sam09@gmail.com>
2024-01-12 16:50:19 +0530

    added solutions of the assignments

--- week-6/1-use-memo/src/App.jsx
+++ week-6/1-use-memo/src/App.jsx
@@ -1,16 +1,17 @@
-import './App.css'
-import { Assignment1 } from './components/Assignment2'
-import { Assignment2 } from './components/Assignment1'
-import Assignment3 from './components/Assignment3'
+import "./App.css";
+import { Assignment1 } from "./components/Assignment1";
+import { Assignment2 } from "./components/Assignment2";
+import Assignment3 from "./components/Assignment3";
+import React from "react";
 
 function App() {
   return (
     <>
       <Assignment1 />
-      {/* <Assignment2 /> */}
-      {/* <Assignment3 /> */}
+      <Assignment2 />
+      <Assignment3 />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
